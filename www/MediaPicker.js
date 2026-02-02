c6413adecc604538272a3b9de295f8984379b5b7
kenfouo <37112508+kenfouo@users.noreply.github.com>
2026-02-02 20:13:54 +0100

    Enhance MediaPicker methods with callback support
    Updated getMedias and getExifForKey methods to support success and error callbacks in addition to Promises.

--- www/MediaPicker.js
+++ www/MediaPicker.js
@@ -6,23 +6,40 @@ function MediaPicker() { }
  * @param {number} opts.selectionLimit - max number of medias
  * @param {boolean} opts.showLoader - show overlay loader
  */
-MediaPicker.prototype.getMedias = function (opts = {}) {
-    return new Promise(function (resolve, reject) {
-        cordova.exec(resolve, reject, 'MediaPicker', 'getMedias', [opts]);
-    });
+MediaPicker.prototype.getMedias = function (opts = {}, successCallback, errorCallback) {
+
+    if (typeof successCallback == 'function' && typeof successCallback == 'function') {
+
+        return cordova.exec(successCallback, errorCallback, 'MediaPicker', 'getMedias', [opts]);
+    }
+    else {
+        return new Promise(function (resolve, reject) {
+            cordova.exec(resolve, reject, 'MediaPicker', 'getMedias', [opts]);
+        });
+    }
 };
 
 /**
- * Get Exif data for a specific file
- * @param {string} fileUri - The file URI (file://...)
- * @param {string} [key] - Optional specific Exif tag name
- */
-MediaPicker.prototype.getExifForKey = function (fileUri, key = null) {
-    return new Promise(function (resolve, reject) {
-        // On passe fileUri et key (qui peut être null)
-        cordova.exec(resolve, reject, 'MediaPicker', 'getExifForKey', [fileUri, key]);
-    });
+ * Get Exif data
+ * @param {string} fileUri - L'URI du fichier
+ * @param {string} key - La clé spécifique (ou null)
+ * @param {function} successCallback - Fonction en cas de succès
+ * @param {function} errorCallback - Fonction en cas d'erreur
+*/
+MediaPicker.prototype.getExifForKey = function (fileUri, key = null, successCallback, errorCallback) {
+
+    if(typeof successCallback == 'function' && typeof successCallback == 'function'){
+
+        return cordova.exec(successCallback, errorCallback, 'MediaPicker', 'getExifForKey', [fileUri, key]);
+    }
+    else{
+        return new Promise(function (resolve, reject) {
+            // On passe fileUri et key (qui peut être null)
+            cordova.exec(resolve, reject, 'MediaPicker', 'getExifForKey', [fileUri, key]);
+        });
+    }
 };
+
 
 module.exports = new MediaPicker();
 module.exports.MediaPicker = module.exports;
